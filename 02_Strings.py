a9c8f335ace57c64b2ddbb8ccf939386e4d118f6
TariqMBooker <55111113+TariqMBooker@users.noreply.github.com>
2019-09-12 11:45:39 -0400

    Update 02_Strings.py

--- 02_Strings.py
+++ 02_Strings.py
@@ -7,6 +7,6 @@ print(message2)
 message3 = "If I say meet me, you'd better do what I say before you get hurt."
 print(message3)
 
-message4 = """HURT!!! I'm on my way now. I'll be there faster then you can say, "Right after I go to the movies and out to eat.""""
+message4 = """HURT!?! I'm on my way now. I'll be there faster then you can say, "Right after I go to the movies and out to eat." Talkin bout some hurt."""
 print(message4)
 
