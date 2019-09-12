a1275938108cc5baf55e6c5abc013ad8f40e84e3
TariqMBooker <55111113+TariqMBooker@users.noreply.github.com>
2019-09-12 11:41:53 -0400

    Strings

---
+++ 02_Strings.py
@@ -0,0 +1,12 @@
+message = "Meet me tonight."
+print(message)
+
+message2 = 'I\'m going out tonight.'
+print(message2)
+
+message3 = "If I say meet me, you'd better do what I say before you get hurt."
+print(message3)
+
+message4 = """HURT!!! I'm on my way now. I'll be there faster then you can say, "Right after I go to the movies and out to eat.""""
+print(message4)
+
