b14bdab83977d3b10e225366fdf41cf7a063ceee
TariqMBooker <55111113+TariqMBooker@users.noreply.github.com>
2019-09-12 12:34:11 -0400

    Update 03_Numbers.py

--- 03_Numbers.py
+++ 03_Numbers.py
@@ -4,15 +4,15 @@ a = 2
 print(a)
 type(a)
 
-#below is the max integer for the computer/system that I am currently using
-#b = 9223372036854775807
+#below is the max integer for the computer/system that I am currently using 
+#b = 9223372036854775807 
 
-#c = 9223372036854775808
-#print(c)
-#type(c)
+#c = 9223372036854775808 
+#print(c) 
+#type(c)  
 
-#d = -c
-#type(d)
+#d = -c 
+#type(d) 
 #print(d)
 
 e = 2.718281828
